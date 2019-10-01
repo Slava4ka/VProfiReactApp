64cb88d90d0173eacd8849fc792cebc58e714a88
Slava <Mr-93@mail.ru>
2019-10-01 16:11:44 +0300

    Updates

---
+++ precache-manifest.b64af797b7cd83b79a2cd6480e4e5432.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "757092b85ebcedbb0a8e6fa738100e03",
+    "url": "/VProfiReactApp/index.html"
+  },
+  {
+    "revision": "3c3b68f099bbc5de4a91",
+    "url": "/VProfiReactApp/static/css/main.37f1a0d4.chunk.css"
+  },
+  {
+    "revision": "2c4842ba8950d984e7d4",
+    "url": "/VProfiReactApp/static/js/2.1bcaa749.chunk.js"
+  },
+  {
+    "revision": "3c3b68f099bbc5de4a91",
+    "url": "/VProfiReactApp/static/js/main.b84de0cd.chunk.js"
+  },
+  {
+    "revision": "da78e12765771c84cfbc",
+    "url": "/VProfiReactApp/static/js/runtime-main.6fc3858d.js"
+  }
+]);
